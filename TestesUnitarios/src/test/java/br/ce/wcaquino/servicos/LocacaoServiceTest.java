51e92617984719b75644af1685a8f2c8543b9f3f
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-17 21:27:12 -0300

    Testes parametrizáveis

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -104,53 +104,6 @@ public class LocacaoServiceTest {
 	}
 	
 	@Test
-	public void devePagar75PctNoFilme3() throws FilmeSemEstoqueException, LocadoraException{
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), new Filme("Filme 3", 2, 4.0));
-		
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		assertThat(resultado.getValor(), is(11.0));
-	}
-	
-	@Test
-	public void devePagar50PctNoFilme4() throws FilmeSemEstoqueException, LocadoraException{
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(
-				new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0));
-		
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		assertThat(resultado.getValor(), is(13.0));
-	}
-	
-	@Test
-	public void devePagar25PctNoFilme5() throws FilmeSemEstoqueException, LocadoraException{
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(
-				new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), 
-				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0),
-				new Filme("Filme 5", 2, 4.0));
-		
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		assertThat(resultado.getValor(), is(14.0));
-	}
-	
-	@Test
-	public void devePagar0PctNoFilme6() throws FilmeSemEstoqueException, LocadoraException{
-		Usuario usuario = new Usuario("Usuario 1");
-		List<Filme> filmes = Arrays.asList(
-				new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0), 
-				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0),
-				new Filme("Filme 5", 2, 4.0), new Filme("Filme 6", 2, 4.0));
-		
-		Locacao resultado = service.alugarFilme(usuario, filmes);
-		
-		assertThat(resultado.getValor(), is(14.0));
-	}
-
-	@Test
 	public void deveDevolverNaSegundaAoAlugarNoDomingo() throws FilmeSemEstoqueException, LocadoraException {
 		assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
 		
