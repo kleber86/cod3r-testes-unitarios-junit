ad642d68e6f53d5a3e2204791ed456e2780c0084
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-23 21:31:12 -0300

    Spy

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -5,6 +5,7 @@ import br.ce.wcaquino.exceptions.NaoPodeDividirPorZero;
 public class Calculadora {
 
 	public int somar(int a, int b) {
+		System.out.println("Estou executando o metodo: somar");
 		return a + b;
 	}
 
@@ -21,5 +22,9 @@ public class Calculadora {
 
 	public int divide(String a, String b) {
 		return Integer.valueOf(a) / Integer.valueOf(b);
+	}
+	
+	public void imprimir() {
+		System.out.println("Passei aqui");
 	}
 }
--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -2,14 +2,49 @@ package br.ce.wcaquino.servicos;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import br.ce.wcaquino.entidades.Locacao;
 
 public class CalculadoraMockTest {
 
+	@Mock
+	private Calculadora calcMock;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Mock
+	private EmailService emailService;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void deveMostrarDiferencaEntreMockSpy() {
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(5);
+		//Mockito.when(calcSpy.somar(1, 2)).thenReturn(5);
+		Mockito.doReturn(5).when(calcSpy).somar(1, 2);
+		Mockito.doNothing().when(calcSpy).imprimir();
+		
+		System.out.println(calcMock.somar(1, 2));
+		System.out.println(calcSpy.somar(1, 2));
+		
+		System.out.println("Mock");
+		calcMock.imprimir();
+		
+		System.out.println("Spy");
+		calcSpy.imprimir();
+	}
+	
 	@Test
 	public void teste() {
 		Calculadora calc = Mockito.mock(Calculadora.class);
